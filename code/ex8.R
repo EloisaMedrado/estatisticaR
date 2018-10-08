a2f15dbc09407741537d244c55f4a1bc19ffceb3
Eloisa Teixeira Medrado <eloisamedrado_11@hotmail.com>
2018-10-08 20:06:03 -0300

    Finalizando exercicio 8.

--- code/ex8.R
+++ code/ex8.R
@@ -13,3 +13,25 @@ library(xlsx)
 #Lendo planilha
 ex8 <- read.xlsx("exercicio8.xls", sheetName = "Plan1")
 head(ex8)
+
+#Break
+brk<-seq(4,4,1)
+
+#Frequenciando dados
+table(cut(ex8$AlturaPacientes, breaks=brk, right=F)) 
+
+#Definindo localização graficos
+setwd("C:\\projetoR\\")
+
+#Salvando imagem grafico
+png(filename = "graficos/ex8_histograma.png", width = 480, height = 480)
+
+#Plotando histograma
+hist(ex8$AlturaPacientes, main="Histograma Alturas Pacientes",
+     xlab="Alturas", ylab="Densidade",
+     col=c("red"), col.main="darkgray", 
+     right=F, breaks=brk)
+
+#Finalizando
+dev.off()
+
